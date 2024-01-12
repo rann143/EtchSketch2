bb8af57dfe8da5955c41917ba9251936f82109a7
Daniel <roderm99@gmail.com>
2024-01-12 10:03:29 -0500

    hard code generated div squares to fit container

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const container = document.querySelector(".container");
 
 for (i = 0; i < 256; i++) {
     //generate new div
-    const box = document.createElement('div');
+    const square = document.createElement('div');
     //style each div
-    box.setAttribute("style", "border: solid 1px; border-color: black; width: 10px; height: 10px;");
+    square.setAttribute("style", 
+    "border: solid 1px;\
+    border-color: black;\
+    padding: 0;\
+    margin: 0;\
+    box-sizing: border-box");
+    square.style.width = "35px";
+    square.style.height = "35px";
     //append to container
-    container.appendChild(box);
+    container.appendChild(square);
 }
+
+// Get container Width (560px)
+// Get container Height (560px)
+
+// Generate div squares to fill the space
